2c73613b9d89108575c0fe6d429550513f3b0210
Dezomorph <qonserwhat@gmail.com>
2022-09-05 20:35:31 +0300

    Added test

--- src/test/java/com/DemoqaFormTest.java
+++ src/test/java/com/DemoqaFormTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -13,7 +15,6 @@ public class DemoqaFormTest {
     @BeforeAll
     static void configure() {
         Configuration.baseUrl = "https://demoqa.com";
-//        Configuration.timeout = 10000; // 10 seconds
         Configuration.browserSize = "1920x1080";
     }
 
@@ -23,14 +24,65 @@ public class DemoqaFormTest {
         $("#firstName").setValue("Human");
         $("#lastName").setValue("Humanov");
         $("#userEmail").setValue("hum@nov.com");
-        $("#male").click();
-        $("#dateOfBirthInput").setValue("12.03.2003");
- //       $("subjects").setValue();
-        $("#reading").click();
-        //$("piecture").setValue() //downloading picture
+        $("[for = gender-radio-3]").click();
+        $("#dateOfBirthInput").click();
+        $("#userNumber").setValue("9080706050");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("2003");
+        $(".react-datepicker__day--013").click();
+        $("#subjectsInput").setValue("Biology").pressEnter();
+        $("[for = hobbies-checkbox-1]").click();
+        $("[for = hobbies-checkbox-2]").click();
+        $("[for = hobbies-checkbox-3]").click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/mem.jpg"));
         $("#currentAddress").setValue("Some address 1");
-        $("#permanentAddress").setValue("Another address 1");
+        $("#state .css-1wa3eu0-placeholder").click();
+        $("#react-select-3-option-1").click();
+        $("#city .css-1wa3eu0-placeholder").click();
+        $("#react-select-4-option-1").click();
         $("#submit").click();
+
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //$("#output #name").shouldHave(text("Egor"));
 //        $("#output").$("#name").shouldHave(text("Egor"));
